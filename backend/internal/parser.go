565fbbb91c4c1129e2f43d84c37f3aad7d163c11
nyunja <poptiff@gmail.com>
2025-03-22 22:07:58 +0300

    feat: Implement PDF text extraction and file download
    This commit introduces functionality to extract text content from PDF files and provides a handler for downloading the generated tailored CV.
    
    - Added `extractTextFromPDF` function to extract text from PDF files using the `pdfcpu` library.
        - This function takes a file path as input and returns the extracted text as a string.
        - Implemented error handling for file reading and content extraction.
        - Uses a temporary file to store the extracted text before reading it.
        - Cleans up the temporary file after extraction.
    - Added `downloadPDFHandler` function to serve the tailored CV as a downloadable PDF file.
        - This function uses `c.File()` to send the `tailored_cv.pdf` file to the client.
    - Updated the `upload.go` file to use the `extractTextFromPDF` function to extract text from the uploaded CV and job description files.
    - Updated the `upload.go` file to save the tailored CV as a pdf file.
    - Updated the `main.go` file to add a route for the `downloadPDFHandler` function.
    
    These changes enable the application to process PDF files and provide the tailored CV as a downloadable PDF.

---
+++ backend/internal/parser.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+)
+
+// Extract text from PDF using pdfcpu
+func extractTextFromPDF(filePath string) string {
+	tmpFile := "extract_text.txt"
+	defer os.Remove(tmpFile)
+
+	err := api.ExtractContentFile(filePath, tmpFile, nil, nil)
+	if err != nil {
+		log.Println("Error extracting content from PDF:", err)
+		return ""
+	}
+	content, err := os.ReadFile(tmpFile)
+	if err != nil {
+		log.Println("Error reading temp file:", err)
+		return ""
+	}
+	return strings.TrimSpace(string(content))
+}
+
+// Handlers to Download Files
+func downloadPDFHandler(c *gin.Context) {
+	c.File("output/tailored_cv.pdf")
+}
